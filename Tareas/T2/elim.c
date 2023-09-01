aab2946eac75526d0871f21b12508828d7b7f4b5
Jimol1711 <juan.molina.l@ug.uchile.cl>
2023-09-01 12:53:41 -0400

    Más cambios

--- Tareas/T2/elim.c
+++ Tareas/T2/elim.c
@@ -5,9 +5,18 @@
 #include "elim.h"
 
 void eliminar(char *str, char *pat) {
-  ...
+  //...
+  for (int i=0; i<strlen(str)-1; i++) {
+    char *caracter_str = str + i;
+    for (int j=0; j<strlen(pat)-1; j++){
+    char *caracter_pat = pat + j;
+    if (caracter_str == caracter_pat){
+      
+    }
+    }
+  }
 }
 
 char *eliminados(char *str, char *pat) {
-  ...
+  //...
 }
