911f916b7b0f8f0937a77d745ad748777e06f690
Jimol1711 <juan.molina.l@ug.uchile.cl>
2023-09-02 21:58:09 -0400

    Se agrega el debugger

--- Tareas/T2/elim.c
+++ Tareas/T2/elim.c
@@ -5,10 +5,14 @@
 #include "elim.h"
 
 void eliminar(char *str, char *pat) {
- 
+  while (*str) {
+    if (*pat == *str)
+      printf(":D\n");
+    str++;
+  }
 }
 
-
 char *eliminados(char *str, char *pat) {
-
+  char *ans = malloc(0);
+  return ans;
 }
