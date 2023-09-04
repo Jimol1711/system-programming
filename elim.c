2132fa7279ae342f7cfc8fc60fa3b679d4d87560
Jimol1711 <juan.molina.l@ug.uchile.cl>
2023-09-04 10:20:07 -0300

    en busqueda de eficiencia

--- elim.c
+++ elim.c
@@ -27,65 +27,20 @@ void eliminar(char *str, char *pat) {
 }
 
 char *eliminados(char *str, char *pat) {
-#if 0
-    // Variables para contar la longitud del resultado
-    int result_length = 0;
-    char *temp = str;
-
-    // Calcular la longitud del resultado
-    while (*temp) {
-        char *temp2 = pat;
-        int match = 1;
-        
-        // Comprobar si se encuentra 'pat' a partir de la posición actual de 'temp'
-        while (*temp2) {
-            if (*temp != *temp2) {
-                match = 0;
-                break;
-            }
+#if 1
+    char *dest = str;
+    char *temp;  
+    int contador = 0; // Se cuenta el número de veces que aparece el patrón   
+    while (*str) {
+        temp = pat;
+        while (*str == *temp && *temp != '\0') {
+            str++;
             temp++;
-            temp2++;
         }
-
-        if (!match) {
-            result_length++;
+        if (*temp == '\0') {
+            contador += 1;
         }
     }
-
-    // Asignar memoria dinámica para el resultado
-    char *result = (char *)malloc(result_length + 1); // +1 para el carácter nulo
-
-    // Copiar los caracteres que no coinciden con 'pat' en 'result'
-    temp = str;
-    char *dest = result;
-    
-    while (*temp) {
-        char *temp2 = pat;
-        int match = 1;
-        
-        // Comprobar si se encuentra 'pat' a partir de la posición actual de 'temp'
-        while (*temp2) {
-            if (*temp != *temp2) {
-                match = 0;
-                break;
-            }
-            temp++;
-            temp2++;
-        }
-
-        if (!match) {
-            *dest = *temp;
-            dest++;
-        }
-        else {
-            temp++;
-        }
-    }
-
-    // Agregar un carácter nulo al final de 'result'
-    *dest = '\0';
-
-    return result;
 #endif
     char *copia_str = malloc(strlen(str)+1);
     strcpy(copia_str,str);
@@ -94,7 +49,7 @@ char *eliminados(char *str, char *pat) {
 
     char *nuevo_str = malloc(strlen(copia_str)+1);
     strcpy(nuevo_str,copia_str);
-    
+
     return nuevo_str;
 
     free(copia_str);
