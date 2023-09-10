a179dd2a37a3d63787116a046abda4ca0e0d43ff
Jimol1711 <juan.molina.l@ug.uchile.cl>
2023-09-10 17:31:05 -0300

    Muchos intentos

--- Random/PruebaT2.c
+++ Random/PruebaT2.c
@@ -3,28 +3,143 @@
 #include <string.h>
 
 void eliminar(char *str, char *pat) {
+    // Se crea un puntero de destino para ir sobreescribiendo el string y uno temporal para recorrer el patrón
     char *dest = str;
-    char *temp;       
     while (*str) {
-        temp = pat;
-        int coincidencia = 1;
-        while (*str == *temp && *temp != '\0') {
-            str++;
-            temp++;
+        // Si los caracteres coinciden, avanzamos en ambos strings
+        if (*str == *pat) {
+            char *temp_str = str;
+            char *temp_pat = pat;
+            int coincidencia = 1;
+            
+            // Comparamos los caracteres de str y pat hasta que pat llegue a su fin
+            while (*temp_pat != '\0') {
+                if (*temp_str != *temp_pat) { 
+                    coincidencia = 0;
+                    break;
+                }
+                temp_str++;
+                temp_pat++;
+            }
+            
+            // Si hay coincidencia, avanzamos el puntero en str
+            if (coincidencia) {
+                str = temp_str;
+            }
         }
-        if (*temp == '\0') {
-            coincidencia = 0;
-        }
-        if (coincidencia) {
-            *dest = *str;
-            dest++;
-            str++;
-        }
+        
+        // Copiamos el caracter actual de str al destino y avanzamos los punteros
+        *dest = *str;
+        dest++;
+        str++;
     }
+    
     *dest = '\0';
 }
 
 char *eliminados(char *str, char *pat) {
+    int contador = 0;
+    char *strcontar = str;
+    while (*strcontar) {
+        // Si los caracteres coinciden, avanzamos en ambos strings
+        if (*strcontar == *pat) {
+            char *temp_str = strcontar;
+            char *temp_pat = pat;
+            int coincidencia = 1;
+            
+            // Comparamos los caracteres de str y pat hasta que pat llegue a su fin
+            while (*temp_pat != '\0') {
+                if (*temp_str != *temp_pat) { 
+                    coincidencia = 0;
+                    break;
+                }
+                temp_str++;
+                temp_pat++;
+            }
+            
+            // Si hay coincidencia, avanzamos el puntero en str
+            if (coincidencia) {
+                contador++;
+            }
+        }
+        strcontar++;
+    }
+
+    // Variable dinámica con malloc
+    char *str_nuevo = malloc(strlen(str)-(contador*strlen(pat))+1);
+    char *str_nuevo_nuevo = str_nuevo;
+    char *str_crear = str;
+    // Se crea un puntero de destino para ir sobreescribiendo el string y uno temporal para recorrer el patrón
+    while (*str_crear) {
+        // Si los caracteres coinciden, avanzamos en ambos strings
+        if (*str_crear == *pat) {
+            char *temp_str = str_crear;
+            char *temp_pat = pat;
+            int coincidencia = 1;
+            
+            // Comparamos los caracteres de str y pat hasta que pat llegue a su fin
+            while (*temp_pat != '\0') {
+                if (*temp_str != *temp_pat) { 
+                    coincidencia = 0;
+                    break;
+                }
+                temp_str++;
+                temp_pat++;
+            }
+            
+            // Si hay coincidencia, avanzamos el puntero en str
+            if (coincidencia) {
+                str_crear = temp_str;
+            }
+        }
+        
+        // Copiamos el caracter actual de str al destino y avanzamos los punteros
+        *str_nuevo_nuevo = *str_crear;
+        str_nuevo_nuevo++;
+        str_crear++;
+    }
+    
+    *str_nuevo_nuevo= '\0';
+    
+    return str_nuevo;
+#if 0
+    int largo = 0;
+    char *dest = str;
+    size_t patLen = strlen(pat);
+
+    while (*str) {
+        if (strncmp(str, pat, patLen) == 0) {
+            continue; // Saltar el patrón en str
+        } else {
+            dest++;
+            str++;
+            largo++;
+        }
+    }
+    *dest = '\0';
+
+    char *str_copy = malloc(strlen(str)+1);
+    strcpy(str_copy,str);
+
+    char *dest2 = str_copy;
+    size_t patLen2 = strlen(pat);
+
+    while (*str) {
+        if (strncmp(str_copy, pat, patLen2) == 0) {
+            str += patLen2; // Saltar el patrón en str
+        } else {
+            *dest2 = *str_copy;
+            dest2++;
+            str++;
+        }
+    }
+    *dest2 = '\0';
+
+    char *str_copy2 = malloc(strlen(str)-(largo*strlen(pat))+1);
+    strcpy(str_copy2,str_copy);
+    free(str_copy);
+    return str_copy2;
+#endif
 #if 0
     char *temp;  
     int contador = 0;     
@@ -67,6 +182,7 @@ char *eliminados(char *str, char *pat) {
 
     return nuevo_string;
 #endif
+#if 0
     int result_length = 0;
     const char* str_ptr = str;
     
@@ -112,12 +228,19 @@ char *eliminados(char *str, char *pat) {
     
     *dest = '\0'; // Null-terminate the result string
     return result;
+#endif
+#if 0
+    char *str_copia = malloc(strlen(str)+1);
+    strcpy(str_copia,str);
+    eliminar(str_copia,pat);
+    return str_copia;
+#endif
 }
 
 int main() {
     // Eliminar
-    char str1[] = "abcabcabcd";
-    char pat1[] = "abcabc";
+    char str1[] = "111011001";
+    char pat1[] = "10";
 
     printf("String original eliminar: %s\n", str1);
     printf("%d\n",strlen(str1));
@@ -129,8 +252,8 @@ int main() {
 
     printf("\n");
     // Eliminados
-    char str2[] = "aaacdbbb";
-    char pat2[] = "cd";
+    char str2[] = "111011001";
+    char pat2[] = "10";
 
     printf("String original eliminados: %s\n", str2);
     printf("%d\n",strlen(str2));
--- elim.c
+++ elim.c
@@ -5,72 +5,237 @@
 #include "elim.h"
 
 void eliminar(char *str, char *pat) {
-    // Se crea un puntero de destino para ir sobreescribiendo el string y uno temporal para recorrer el patrón
+#if 0
     char *dest = str;
-    char *temp;       
+    size_t patLen = strlen(pat);
+
     while (*str) {
-        temp = pat;
-        // La variable coincidencia se usa para determinar cuando se encontró una versión del patrón en el string, y así saltarsela
-        int coincidencia = 1;
-        while (*str == *temp && *temp != '\0') {
-            str++;
-            temp++;
-        }
-        if (*temp == '\0') {
-            coincidencia = 0;
-        }
-        if (coincidencia) {
+        if (strncmp(str, pat, patLen) == 0) {
+            str += patLen; // Saltar el patrón en str
+        } else {
             *dest = *str;
             dest++;
             str++;
         }
     }
+
     *dest = '\0';
+#endif
+#if 1
+    // Se crea un puntero de destino para ir sobreescribiendo el string y uno temporal para recorrer el patrón
+    char *dest = str;
+    while (*str) {
+        // Si los caracteres coinciden, avanzamos en ambos strings
+        if (*str == *pat) {
+            char *temp_str = str;
+            char *temp_pat = pat;
+            int coincidencia = 1;
+            
+            // Comparamos los caracteres de str y pat hasta que pat llegue a su fin
+            while (*temp_pat != '\0') {
+                if (*temp_str != *temp_pat) { 
+                    coincidencia = 0;
+                    break;
+                }
+                temp_str++;
+                temp_pat++;
+            }
+            
+            // Si hay coincidencia, avanzamos el puntero en str
+            if (coincidencia) {
+                str = temp_str;
+            }
+        }
+        
+        // Copiamos el caracter actual de str al destino y avanzamos los punteros
+        *dest = *str;
+        dest++;
+        str++;
+    }
+    
+    *dest = '\0';
+#endif
 }
 
 char *eliminados(char *str, char *pat) {
-    int largo_resultante = 0;
-    const char* str_ptr = str;
-    
-    // Calulamos largo del string resultante, es básicamente lo mismo que eliminar pero solo cuenta el número de caracteres que debería tener el string final
-    while (*str_ptr) {
-        const char* temp = pat;
-        int coincidencia = 1;
-        while (*str_ptr == *temp && *temp != '\0') {
-            str_ptr++;
-            temp++;
+    int contador = 0;
+    char *strcontar = str;
+    while (*strcontar) {
+        // Si los caracteres coinciden, avanzamos en ambos strings
+        if (*strcontar == *pat) {
+            char *temp_str = strcontar;
+            char *temp_pat = pat;
+            int coincidencia = 1;
+            
+            // Comparamos los caracteres de str y pat hasta que pat llegue a su fin
+            while (*temp_pat != '\0') {
+                if (*temp_str != *temp_pat) { 
+                    coincidencia = 0;
+                    break;
+                }
+                temp_str++;
+                temp_pat++;
+            }
+            
+            // Si hay coincidencia, avanzamos el puntero en str
+            if (coincidencia) {
+                contador++;
+            }
         }
-        if (*temp == '\0') {
-            coincidencia = 0;
+        strcontar++;
+    }
+
+    // Variable dinámica con malloc
+    char *str_nuevo = malloc(strlen(str)-(contador*strlen(pat))+1);
+    char *str_nuevo_nuevo = str_nuevo;
+    char *str_crear = str;
+    // Se crea un puntero de destino para ir sobreescribiendo el string y uno temporal para recorrer el patrón
+    while (*str_crear) {
+        // Si los caracteres coinciden, avanzamos en ambos strings
+        if (*str_crear == *pat) {
+            char *temp_str = str_crear;
+            char *temp_pat = pat;
+            int coincidencia = 1;
+            
+            // Comparamos los caracteres de str y pat hasta que pat llegue a su fin
+            while (*temp_pat != '\0') {
+                if (*temp_str != *temp_pat) { 
+                    coincidencia = 0;
+                    break;
+                }
+                temp_str++;
+                temp_pat++;
+            }
+            
+            // Si hay coincidencia, avanzamos el puntero en str
+            if (coincidencia) {
+                str_crear = temp_str;
+            }
         }
-        if (coincidencia) {
-            largo_resultante++;
-            str_ptr++;
-        }
+        
+        // Copiamos el caracter actual de str al destino y avanzamos los punteros
+        *str_nuevo_nuevo = *str_crear;
+        str_nuevo_nuevo++;
+        str_crear++;
     }
     
-    // Se crea variable dinámica para el string resultante
-    char* result = malloc(largo_resultante + 1);
+    *str_nuevo_nuevo= '\0';
     
-    // Se copia el string a la variable dinámica
-    char* dest = result;
-    str_ptr = str; // Acá creo otro puntero para evitar modificar el string original
-    while (*str_ptr) {
-        char* temp = pat;
-        int coincidencia = 1;
-        while (*str_ptr == *temp && *temp != '\0') {
-            str_ptr++;
-            temp++;
-        }
-        if (*temp == '\0') {
-            coincidencia = 0;
-        }
-        if (coincidencia) {
-            *dest = *str_ptr;
+    return str_nuevo;
+#if 0
+    int largo = 0;
+    char *dest = str;
+    size_t patLen = strlen(pat);
+
+    while (*str) {
+        if (strncmp(str, pat, patLen) == 0) {
+            continue; // Saltar el patrón en str
+        } else {
             dest++;
-            str_ptr++;
+            str++;
+            largo++;
         }
-    } 
+    }
     *dest = '\0';
-    return result; // Se devuelve el nuevo string almacenado en la variable dinámica
+
+    char *str_copy = malloc(strlen(str)+1);
+    strcpy(str_copy,str);
+
+    char *dest2 = str_copy;
+    size_t patLen2 = strlen(pat);
+
+    while (*str) {
+        if (strncmp(str_copy, pat, patLen) == 0) {
+            str += patLen2; // Saltar el patrón en str
+        } else {
+            *dest2 = *str_copy;
+            dest2++;
+            str++;
+        }
+    }
+    *dest2 = '\0';
+
+    char *str_copy2 = malloc(strlen(str)-(largo*strlen(pat))+1);
+    strcpy(str_copy2,str_copy);
+    free(str_copy);
+    return str_copy2;
+
+#endif
+#if 0
+    char *str_copia = malloc(strlen(str)+1);
+    strcpy(str_copia,str);
+    // Calculo del largo del string
+    int contador = 0;
+    while (*str_copia) {
+        if (*str_copia == *pat) {
+
+            char *temp_str = str_copia;
+            char *temp_pat = pat;
+            int coincidencia = 1;
+
+            while (*temp_pat != '\0') {
+
+                if (*temp_str != *temp_pat) {
+                    coincidencia = 0;
+                    break;
+                }
+
+            temp_str++;
+            temp_pat++;
+
+            }
+
+        if (coincidencia) {
+            str_copia = temp_str;
+            contador++;
+            }
+
+        }
+        str_copia++;
+    }   
+
+    char *dest = str_copia;
+    while (*str_copia) {
+        
+        if (*str_copia == *pat) {
+            char *temp_str = str_copia;
+            char *temp_pat = pat;
+            int coincidencia = 1;
+            
+            while (*temp_pat != '\0') {
+                if (*temp_str != *temp_pat) {
+                    coincidencia = 0;
+                    break;
+                }
+                temp_str++;
+                temp_pat++;
+            }
+
+            if (coincidencia) {
+                str_copia = temp_str;
+            }
+        }
+        
+        *dest = *str_copia;
+        dest++;
+        str_copia++;
+    }
+    *dest = '\0';
+
+    return str_copia;
+#endif
+#if 0
+    char *str_copy = malloc(strlen(str)+1);
+    strcpy(str_copy,str);
+
+    eliminar(str_copy,pat);
+
+    char *str_copy2 = malloc(strlen(str_copy)+1);
+    strcpy(str_copy2,str_copy);
+
+    free(str_copy);
+
+    return str_copy2;
+#endif
+
 }
