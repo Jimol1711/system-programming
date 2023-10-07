71710d6a69f3a14877ebd160615ac25f7cd247f5
Jimol1711 <juan.molina.l@ug.uchile.cl>
2023-10-07 18:06:04 -0300

    No se calcula el hash que raro

--- definir.c
+++ definir.c
@@ -8,7 +8,7 @@
 // La función agregarDefinición recibe el diccionario, la palabra y su definción y la agrega al diccionario
 int agregarDefinicion(char *nom_dic, char *pal, char *def) {
   FILE *arch = fopen(nom_dic, "r+");
-  char buf[82];
+  char buf[81];
 
   //Diagnóstico de errores usando perror
   if (arch == NULL) {
@@ -22,30 +22,30 @@ int agregarDefinicion(char *nom_dic, char *pal, char *def) {
   fseek(arch, 0, SEEK_SET);
 
   // Calculamos el número de líneas
-  int lineas = size / 82;
+  int lineas = size / 81;
 
   // Calculamos donde debe estar la definición con hash
   int linea_def = hash_string(pal) % lineas;
 
   // Nos movemos a esa línea
-  fseek(arch, linea_def * 82, SEEK_SET);
+  fseek(arch, linea_def * 81, SEEK_SET);
 
   // Buscamos la primera línea vacía con un loop
   while (fgets(buf, 82, arch) != NULL) {
     if (buf[0] == '\n') {
-      fseek(arch, -82, SEEK_CUR);
+      fseek(arch, -81, SEEK_CUR);
       fprintf(arch, "%s:%s\n", pal, def);
       fclose(arch);
       return 0;
     }
-    fseek(arch, 82, SEEK_CUR);
+    fseek(arch, 81, SEEK_CUR);
   }
 
   fseek(arch, 0, SEEK_SET);
 
-  while (fgets(buf, 82, arch) != NULL) {
+  while (fgets(buf, 81, arch) != NULL) {
     if (buf[0] == '\n') {
-      fseek(arch, -82, SEEK_CUR);
+      fseek(arch, -81, SEEK_CUR);
       fprintf(arch, "%s:%s\n", pal, def);
       fclose(arch);
       return 0;
