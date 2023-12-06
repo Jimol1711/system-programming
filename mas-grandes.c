0063476afc8af34ea96bad0af8f98606fc39dcb8
Jimol1711 <juan.molina.l@ug.uchile.cl>
2023-12-06 01:55:39 -0300

    No me sale

--- mas-grandes.c
+++ mas-grandes.c
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <limits.h>
 
 #include "pss.h"
        
@@ -17,49 +18,70 @@ typedef struct {
   off_t size;
 } FileInfo;
 
-// Cola global
+// Variables globales
 Queue *queue;
 int queueSize;
 FileInfo **fileArray;
 
-void addToQueue(const char *filename) {
-    struct stat fileStat;
-
-    if (stat(filename, &fileStat) == 0 && S_ISREG(fileStat.st_mode)) {
-        FileInfo *fileInfo = (FileInfo *)malloc(sizeof(FileInfo));
-        fileInfo->filename = strdup(filename);
-        fileInfo->size = fileStat.st_size;
-        put(queue, (void *)fileInfo);
-    }
-}
-
-int recorrerDir(const char *dir) {
-
-  // Recorrido de los contenidos del directorio en inserción de archivos en la cola
+void recorrerDir(char *nom, Queue *archsQueue) {
+  struct stat st_nom;
+  stat(nom, &st_nom);
   
+  if (S_ISREG(st_nom.st_mode)) {
+    // Es un archivo regular
+    char *str_dup = strdup(nom);
+    off_t newSize = st_nom.st_size;
+    FileInfo *fileInfo = (FileInfo *)malloc(sizeof(FileInfo));
+    fileInfo->filename = str_dup;
+    fileInfo->size = newSize;
+    put(queue,fileInfo);
+  }
+  else if (S_ISDIR(st_nom.st_mode)) {
+    // Es un directorio
+    DIR *dir = opendir(nom);
+    for (struct dirent *entry = readdir(dir);
+         entry != NULL;
+         entry = readdir(dir)) {
+      if (strcmp(entry->d_name, ".")==0 || strcmp(entry->d_name, "..")==0) {
+        continue;
+      }
+      recorrerDir(entry->d_name,queue);
+    }
+    closedir(dir);
+  } else {
+    // Podria ser un dispositivo, un link simbolico, etc.
+    fprintf(stderr, "Archivo %s es de tipo desconocido\n", nom);
+    exit(0);
+  }
 }
 
 // Función de comparación para sortPtrArray
-int compareFileSize(const void *a, const void *b) {
-    const FileInfo *fileA = *(const FileInfo **)a;
-    const FileInfo *fileB = *(const FileInfo **)b;
+int compareFileSize(void *ptr, int a, int b) {
+  FileInfo *fileA = ((FileInfo **)ptr)[a];
+  FileInfo *fileB = ((FileInfo **)ptr)[b];
 
-    // Comparar por tamnno de archivo
-    return fileB->size - fileA->size;
+  // Comparar por tamaño de archivo
+  return (int)(fileB->size - fileA->size);
 }
 
 int main(int argc, char *argv[]) {
-
   queue = makeQueue();
+
+  if (argc != 3) {
+    fprintf(stderr, "Uso: %s <directorio> <n>\n", argv[0]);
+    return 1;
+  }
+
+  recorrerDir(argv[1],queue);
 
   // Tamanno de la cola
   queueSize = queueLength(queue);
 
   // Arreglo para insertar la información de la cola
-  FileInfo **fileArray = (FileInfo **)malloc(queueSize * sizeof(FileInfo *));
+  fileArray = (FileInfo **)malloc(queueSize * sizeof(FileInfo *));
 
   // Transferir los elementos de la cola al arreglo
-  for (int i = 0; i < queueSize; ++i) {
+  for (int i = 0; i < queueSize; i++) {
     fileArray[i] = (FileInfo *)get(queue);
   }
 
@@ -67,15 +89,11 @@ int main(int argc, char *argv[]) {
   sortPtrArray(fileArray, 0, queueSize - 1, compareFileSize);
 
   // Mostrar los n archivos más grandes
-  for (int i = 0; i < atoi(argv[2]) && i < queueSize; ++i) {
+  for (int i = 0; i < atoi(argv[2]); ++i) {
       printf("%s %ld\n", fileArray[i]->filename, fileArray[i]->size);
   }
 
-  // Liberar memoria utilizada
-  for (int i = 0; i < queueSize; ++i) {
-      free(fileArray[i]->filename);
-      free(fileArray[i]);
-  }
+  free(fileArray);
 
   return 0;
 
